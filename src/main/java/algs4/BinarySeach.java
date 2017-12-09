b2bdd6ca82cf90488ff0dfca877c7570368d9050
zhangdong <2319551966@qq.com>
2017-12-09 16:32:11 +0800

    修改代码格式

--- src/main/java/algs4/BinarySeach.java
+++ src/main/java/algs4/BinarySeach.java
@@ -1,5 +1,7 @@
 package algs4;
 
+import edu.princeton.cs.algs4.StdOut;
+
 import java.util.Arrays;
 
 /**
@@ -7,35 +9,36 @@ import java.util.Arrays;
  */
 public class BinarySeach {
     public static void main(String[] args) {
-        int[] a = {13,32,43,15,16,112,33,35,345,77,123,456,777};
+        int[] a = {13, 32, 43, 15, 16, 112, 33, 35, 345, 77, 123, 456, 777};
         printArr(a);
-        int rs = rank(3333,a);
+        int rs = rank(3333, a);
         System.out.println(rs);
     }
 
-    public static int rank(int key ,int[] a){
+    public static int rank(int key, int[] a) {
         Arrays.sort(a);
         printArr(a);
         int lo = 0;
         int hi = a.length - 1;
 //        int mid = lo + (hi - lo) / 2;
         int j = 1;
-        while (lo <= hi){
+        while (lo <= hi) {
             int mid = lo + (hi - lo) / 2;
-            if (key < a[mid]){
+            if (key < a[mid]) {
                 hi = mid - 1;
-            }else if (key > a[mid]){
+            } else if (key > a[mid]) {
                 lo = mid + 1;
-            }else return mid;
+            } else return mid;
         }
         return -1;
     }
 
-    public static void printArr(int[] a){
+    public static void printArr(int[] a) {
         System.out.print("[");
-        for (int i = 0;i<a.length;i++){
-            System.out.print(" "+a[i]+" ");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(" " + a[i] + " ");
         }
-        System.out.println("]");
+//        System.out.println("]");
+        StdOut.println("]");
     }
 }
